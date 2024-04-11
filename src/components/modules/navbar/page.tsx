bd79ff76534ddf5afc0f54b3048ce314d1b99974
marzieh <marziehmonshizade@yahoo.com>
2024-04-12 00:45:00 +0330

    coding navbar

--- src/components/modules/navbar/page.tsx
+++ src/components/modules/navbar/page.tsx
@@ -1,53 +1,87 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FaCartShopping, FaRegHeart, FaShuffle } from "react-icons/fa6";
+import { FaBars, FaCartShopping, FaRegHeart, FaShuffle } from "react-icons/fa6";
+import { MdWbSunny } from "react-icons/md";
 
 type Props = {};
 
 export default function Navbar({}: Props) {
   return (
-    <nav className="flex justify-between items-center bg-orange-200 p-2 text-sm m-6 mx-16">
-      <div>
-        <Image
-          alt="cooffee time"
-          src="/coffee-time-logo.png"
-          width={70}
-          height={70}
-        />
-      </div>
-      <div className="font-bold font-sans">
-        <Link href="/" className="m-2">
-          صفحه اصلی
-        </Link>
-        <Link href="/" className="m-2">
-          فروشگاه
-        </Link>
-        <Link href="/" className="m-2">
-          فروش سازمانی
-        </Link>
-        <Link href="/" className="m-2">
-          وبلاگ
-        </Link>
-        <Link href="/" className="m-2">
-          دیکشنری قهوه
-        </Link>
-        <Link href="/" className="m-2">
-          تماس با ما
-        </Link>
-        <Link href="/" className="m-2">
-          درباره ما
-        </Link>
-        <Link href="/" className="m-2">
-          باشگاه مشتریان
-        </Link>
-        <Link href="/" className="m-2">
-          ورود/عضویت
-        </Link>
-      </div>
-      <div className="flex gap-4">
-        <FaRegHeart className="w-5 h-5" />
-        <FaShuffle className="w-5 h-5" />
-        <FaCartShopping className="w-5 h-5" />
+    <nav className="absolute z-50 w-full">
+      <div className="flex justify-between items-center bg-orange-200 p-2 px-4 text-sm m-6 mx-16 shadow-md">
+        <section className="flex items-center gap-3">
+          <FaBars className="w-5 h-5 text-orange-950 md:hidden inline-flex" />
+          <Image
+            alt="cooffee time"
+            src="/coffee-time-logo.png"
+            width={70}
+            height={70}
+            priority
+          />
+        </section>
+        <section className="font-bold font-sans md:inline-flex hidden">
+          <Link href="/" className="m-2">
+            صفحه اصلی
+          </Link>
+          <Link href="/" className="m-2">
+            فروشگاه
+          </Link>
+          <Link href="/" className="m-2">
+            فروش سازمانی
+          </Link>
+          <Link href="/" className="m-2">
+            وبلاگ
+          </Link>
+          <Link href="/" className="m-2">
+            دیکشنری قهوه
+          </Link>
+          <Link href="/" className="m-2">
+            تماس با ما
+          </Link>
+          <Link href="/" className="m-2">
+            درباره ما
+          </Link>
+          <Link href="/" className="m-2">
+            باشگاه مشتریان
+          </Link>
+          <Link href="/" className="m-2">
+            ورود/عضویت
+          </Link>
+        </section>
+        <section className="flex items-center gap-4">
+          <MdWbSunny className="w-6 h-6 text-orange-950" />
+
+          <button
+            type="button"
+            className="relative inline-flex p-1 items-center text-sm font-medium text-center text-white   focus:ring-4 focus:outline-none  "
+          >
+            <FaRegHeart className="w-5 h-5 text-orange-950" />
+            <span className="sr-only">Notifications</span>
+            <div className="absolute inline-flex items-center justify-center w-5 h-5 p-1 text-xs font-bold text-white bg-orange-900 rounded-full -top-2 -end-2 dark:border-gray-900">
+              0
+            </div>
+          </button>
+          <button
+            type="button"
+            className="relative inline-flex p-1 items-center text-sm font-medium text-center text-white   focus:ring-4 focus:outline-none  "
+          >
+            <FaShuffle className="w-5 h-5 text-orange-950" />
+            <span className="sr-only">Notifications</span>
+            <div className="absolute inline-flex items-center justify-center w-5 h-5 p-1 text-xs font-bold text-white bg-orange-900 rounded-full -top-2 -end-2 dark:border-gray-900">
+              0
+            </div>
+          </button>
+          <button
+            type="button"
+            className="relative inline-flex p-1 items-center text-sm font-medium text-center text-white   focus:ring-4 focus:outline-none  "
+          >
+            <FaCartShopping className="w-5 h-5 text-orange-950" />
+            <span className="sr-only">Notifications</span>
+            <div className="absolute inline-flex items-center justify-center w-5 h-5 p-1 text-xs font-bold text-white bg-orange-900 rounded-full -top-2 -end-2 dark:border-gray-900">
+              0
+            </div>
+          </button>
+        </section>
       </div>
     </nav>
   );
