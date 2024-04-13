c7846c8331711ece68fa1c2f6b7ece12c0bb8b13
marzieh <marziehmonshizade@yahoo.com>
2024-04-13 03:55:32 +0330

    coding navbar drapdown

--- src/components/modules/navbar/page.tsx
+++ src/components/modules/navbar/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FaBars, FaCartShopping, FaRegHeart, FaShuffle } from "react-icons/fa6";
+import {
+  FaAngleDown,
+  FaBars,
+  FaCartShopping,
+  FaRegHeart,
+  FaShuffle,
+} from "react-icons/fa6";
 import { MdWbSunny } from "react-icons/md";
 
 type Props = {};
@@ -8,49 +14,253 @@ type Props = {};
 export default function Navbar({}: Props) {
   return (
     <nav className="absolute z-50 w-full">
-      <div className="flex justify-between items-center bg-orange-200 p-2 px-4 text-sm m-6 mx-16 shadow-md">
-        <section className="flex items-center gap-3">
-          <FaBars className="w-5 h-5 text-orange-950 md:hidden inline-flex" />
-          <Image
-            alt="cooffee time"
-            src="/coffee-time-logo.png"
-            width={70}
-            height={70}
-            priority
-          />
-        </section>
-        <section className="font-bold font-sans md:inline-flex hidden">
-          <Link href="/" className="m-2">
-            صفحه اصلی
-          </Link>
-          <Link href="/" className="m-2">
-            فروشگاه
-          </Link>
-          <Link href="/" className="m-2">
-            فروش سازمانی
-          </Link>
-          <Link href="/" className="m-2">
-            وبلاگ
-          </Link>
-          <Link href="/" className="m-2">
-            دیکشنری قهوه
-          </Link>
-          <Link href="/" className="m-2">
-            تماس با ما
-          </Link>
-          <Link href="/" className="m-2">
-            درباره ما
-          </Link>
-          <Link href="/" className="m-2">
-            باشگاه مشتریان
-          </Link>
-          <Link href="/" className="m-2">
-            ورود/عضویت
-          </Link>
-        </section>
-        <section className="flex items-center gap-4">
-          <MdWbSunny className="w-6 h-6 text-orange-950" />
+      <main className="flex justify-between items-center bg-orange-200 p-1 px-4 text-sm m-6 lg:mx-16 shadow-md rounded-3xl">
+        {/* ......right........ */}
 
+        <div className="flex items-center gap-3">
+          <FaBars className="w-5 h-5 text-orange-950 md:hidden inline-flex" />
+          <Link href="/">
+            <Image
+              alt="cooffee time"
+              src="/coffee-time-logo.png"
+              width={70}
+              height={70}
+              priority
+            />
+          </Link>
+        </div>
+        {/* md:inline-flex hidden */}
+        {/* ......center........ */}
+
+        <ul className="relative font-bold font-sans inline-flex flex-row gap-6">
+          <li>
+            <Link href="#" className="m-2">
+              صفحه اصلی
+            </Link>
+          </li>
+
+          <li className="relative group">
+            <Link
+              href="/store"
+              id="dropdownMenuIconButton"
+              data-dropdown-toggle="dropdownDots"
+              className="inline-flex items-center text-sm font-medium text-center "
+            >
+              فروشگاه
+              <FaAngleDown className="mt-1 group-hover:rotate-180 transition-all" />
+            </Link>
+            <div
+              id="dropdownDots"
+              className="hidden group-hover:block hover:block absolute top-[21px] bg-orange-50  z-10 divide-ybg-orange-50 divide-orange-100 rounded-lg w-44 shadow-sm shadow-orange-600 dark:bg-gray-700 dark:divide-gray-600"
+            >
+              <ul
+                className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdownMenuIconButton"
+              >
+                <li>
+                  <Link
+                    href="/p-user/orders"
+                    className="block px-4 py-2 hover:bg-orange-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    سفارشات
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/p-user/tickets"
+                    className="block px-4 py-2 hover:bg-orange-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    تیکت های پشتیبانی
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </li>
+
+          {/* <li>
+            <Link href="#" className="m-2">
+              فروش سازمانی
+            </Link>
+          </li> */}
+
+          {/* <li>
+            <Link href="#" className="m-2">
+              دیکشنری قهوه
+            </Link>
+          </li> */}
+
+          {/* <li>
+            <Link href="#" className="m-2">
+              باشگاه مشتریان
+            </Link>
+          </li> */}
+
+          <li className="relative group">
+            <Link
+              href="/weblog"
+              id="dropdownMenuIconButton"
+              data-dropdown-toggle="dropdownDots"
+              className="inline-flex items-center text-sm font-medium text-center "
+            >
+              وبلاگ
+              <FaAngleDown className="mt-1 group-hover:rotate-180 transition-all" />
+            </Link>
+            <div
+              id="dropdownDots"
+              className="hidden group-hover:block hover:block absolute top-[21px] bg-orange-50  z-10 divide-ybg-orange-50 divide-orange-100 rounded-lg w-44 shadow-sm shadow-orange-600 dark:bg-gray-700 dark:divide-gray-600"
+            >
+              <ul
+                className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdownMenuIconButton"
+              >
+                <li>
+                  <Link
+                    href="/p-user/orders"
+                    className="block px-4 py-2 hover:bg-orange-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    سفارشات
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/p-user/tickets"
+                    className="block px-4 py-2 hover:bg-orange-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    تیکت های پشتیبانی
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <Link href="#" className="flex items-center">
+              قوانین
+            </Link>
+          </li>
+
+          <li>
+            <Link href="#" className="m-2">
+              تماس با ما
+            </Link>
+          </li>
+
+          <li className="relative group">
+            <Link
+              href="/about-us"
+              id="dropdownMenuIconButton"
+              data-dropdown-toggle="dropdownDots"
+              className="inline-flex items-center text-sm font-medium text-center "
+            >
+              درباره ما
+              <FaAngleDown className="mt-1 group-hover:rotate-180 transition-all" />
+            </Link>
+            <div
+              id="dropdownDots"
+              className="hidden group-hover:block hover:block absolute top-[21px] bg-orange-50  z-10 divide-ybg-orange-50 divide-orange-100 rounded-lg w-44 shadow-sm shadow-orange-600 dark:bg-gray-700 dark:divide-gray-600"
+            >
+              <ul
+                className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdownMenuIconButton"
+              >
+                <li>
+                  <Link
+                    href="/p-user/orders"
+                    className="block px-4 py-2 hover:bg-orange-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    سفارشات
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/p-user/tickets"
+                    className="block px-4 py-2 hover:bg-orange-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    تیکت های پشتیبانی
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/p-user/comments"
+                    className="block px-4 py-2 hover:bg-orange-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    کامنت ها
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </li>
+
+          <li>
+            <Link href="/login_register" className="m-2">
+              ورود/عضویت
+            </Link>
+          </li>
+          {/* if user login */}
+          <li className="relative group">
+            <Link
+              href="/p-user"
+              id="dropdownMenuIconButton"
+              data-dropdown-toggle="dropdownDots"
+              className="inline-flex items-center text-sm font-medium text-center "
+            >
+              حساب کاربری
+              <FaAngleDown className="mt-1 group-hover:rotate-180 transition-all" />
+            </Link>
+            <div
+              id="dropdownDots"
+              className="hidden group-hover:block hover:block absolute top-[21px] bg-orange-50  z-10 divide-ybg-orange-50 divide-orange-100 rounded-lg  w-44 shadow-sm shadow-orange-600 dark:bg-gray-700 dark:divide-gray-600"
+            >
+              <ul
+                className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdownMenuIconButton"
+              >
+                <li>
+                  <Link
+                    href="/p-user/orders"
+                    className="block px-4 py-2 hover:bg-orange-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    سفارشات
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/p-user/tickets"
+                    className="block px-4 py-2 hover:bg-orange-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    تیکت های پشتیبانی
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/p-user/comments"
+                    className="block px-4 py-2 hover:bg-orange-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    کامنت ها
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/p-user/favorites"
+                    className="block px-4 py-2 hover:bg-orange-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    علاقه مندی ها
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/p-user/account-details"
+                    className="block px-4 py-2 hover:bg-orange-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    جزئیات اکانت{" "}
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+        {/* ......left........ */}
+        <div className="flex items-center gap-3">
+          <MdWbSunny className="w-6 h-6 text-orange-950" />
           <button
             type="button"
             className="relative inline-flex p-1 items-center text-sm font-medium text-center text-white   focus:ring-4 focus:outline-none  "
@@ -81,8 +291,8 @@ export default function Navbar({}: Props) {
               0
             </div>
           </button>
-        </section>
-      </div>
+        </div>
+      </main>
     </nav>
   );
 }
