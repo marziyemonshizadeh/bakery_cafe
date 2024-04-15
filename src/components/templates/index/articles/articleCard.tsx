7d000887854a34b6746376a02da3a8bf887e11ef
marzieh <marziehmonshizade@yahoo.com>
2024-04-16 01:41:29 +0330

    coding article card

---
+++ src/components/templates/index/articles/articleCard.tsx
@@ -0,0 +1,41 @@
+import { FaShareAlt } from "react-icons/fa";
+import { FaUserLarge } from "react-icons/fa6";
+import { TbMessage } from "react-icons/tb";
+
+type Props = {
+  img: string;
+  date: number;
+  month: string;
+  title: string;
+  writer: string;
+};
+
+export default function Article({ img, date, month, title, writer }: Props) {
+  return (
+    <div
+      className="bg-no-repeat text-lg text-white p-3 bg-cover flex flex-col justify-between"
+      style={{
+        backgroundImage: `url("/${img}")`,
+        width: "350px",
+        height: "350px",
+        objectFit: "cover",
+      }}
+    >
+      <div className="bg-slate-50 rounded-full text-black w-16 h-16 flex flex-col items-center">
+        <span>{date}</span>
+        <span>{month}</span>
+      </div>
+      <div className="flex flex-col items-center gap-3">
+        <h2 className="text-2xl font-extrabold">{title}</h2>
+        <div className="flex items-center gap-4">
+          <div className="flex items-center gap-1">
+            <FaUserLarge />
+            {writer}
+          </div>
+          <TbMessage />
+          <FaShareAlt />
+        </div>
+      </div>
+    </div>
+  );
+}
