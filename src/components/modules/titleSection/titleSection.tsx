5dd5c335424d6798570b0f33bf585f5113588c6d
marzieh <marziehmonshizade@yahoo.com>
2024-04-16 01:39:32 +0330

    change title section text color

--- src/components/modules/titleSection/titleSection.tsx
+++ src/components/modules/titleSection/titleSection.tsx
@@ -5,8 +5,8 @@ type Props = {
 export default function TitleSection({ title }: Props) {
   return (
     <div data-aos="fade-down" className="relative">
-      <div className="absolute left-1/2 -ml-0.5 w-[1.5px] h-28 bg-orange-900 mt-10"></div>
-      <p className="pt-40 text-orange-900 flex justify-center items-center text-lg font-bold">
+      <div className="absolute left-1/2 -ml-0.5 w-[1.5px] h-28 bg-[#897D5E] mt-10"></div>
+      <p className="pt-40 text-[#897D5E] flex justify-center items-center text-lg font-bold">
         {title}
       </p>
     </div>
