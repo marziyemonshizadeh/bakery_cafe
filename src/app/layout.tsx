74c4e570c3f3fa4173fb5299a4ec9adbf9d99d22
marzieh <marziehmonshizade@yahoo.com>
2024-04-29 01:29:52 +0330

    make layout.tsx server component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
-"use client";
+import ScrollToTop from "@/components/modules/scrollToTop/scrollToTop";
 import AosInit from "@/utils/aos";
-import ScrollToTop from "@/utils/scrollToTop";
-import { ThemeProvider, useTheme } from "next-themes";
+import ThemeProv from "@/utils/theme";
 import "./globals.css";
 
 export const metadata = {
@@ -18,17 +17,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const { resolvedTheme } = useTheme();
   return (
-    <html lang="fa" dir="rtl">
+    <html lang="fa" dir="rtl" suppressHydrationWarning={true}>
       <body>
-        <ThemeProvider attribute="class">
-          <div className={`relative ${resolvedTheme === "dark" ? "dark" : ""}`}>
+        <ThemeProv>
+          {/* static */}
+          <div className="static scrollbar-thin h-screen  scrollbar-thumb-[#413a2d] dark:scrollbar-thumb-[#D1B48C] scrollbar-track-[#D1B48C] dark:scrollbar-track-[#413a2d]  overflow-y-scroll overflow-x-hidden">
             <AosInit />
             {children}
             <ScrollToTop />
           </div>
-        </ThemeProvider>
+        </ThemeProv>
       </body>
     </html>
   );
