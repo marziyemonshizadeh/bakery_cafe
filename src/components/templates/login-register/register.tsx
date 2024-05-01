eb7ad2a18b2a39ca983c028adb68e28df498c08b
marzieh <marziehmonshizade@yahoo.com>
2024-05-02 00:49:55 +0330

    add : coding register page

--- src/components/templates/login-register/register.tsx
+++ src/components/templates/login-register/register.tsx
@@ -1,8 +1,19 @@
 "use client";
 import { registerFormValues } from "@/types/typings";
+import {
+  validateEmail,
+  validatePassword,
+  validatePhone,
+  validateUserName,
+} from "@/utils/auth";
+import { showSwal } from "@/utils/helpers";
 import Link from "next/link";
 import { SubmitHandler, useForm } from "react-hook-form";
-export default function Register() {
+
+type Props = {
+  showloginForm: () => void;
+};
+export default function Register({ showloginForm }: Props) {
   const {
     register,
     reset,
@@ -10,49 +21,64 @@ export default function Register() {
     formState: { errors, isDirty, isValid },
   } = useForm<registerFormValues>();
   const onSubmit: SubmitHandler<registerFormValues> = async (data: any) => {
-    await fetch("api/messages", {
+    console.log(isDirty);
+
+    if (!data.userName.trim()) {
+      return showSwal("نام را وارد بکنید", "error", "تلاش مجدد");
+    }
+
+    const isValidUserName = validateUserName(data.userName);
+    if (!isValidUserName) {
+      return showSwal(
+        "نام کاربری باید حداقل 5 و حداکثر 16 کاراکتر داشته باشد",
+        "error",
+        "تلاش مجدد "
+      );
+    }
+
+    const isValidPhone = validatePhone(data.phoneNumber);
+    if (!isValidPhone) {
+      return showSwal("شماره تماس وارد شده معتبر نیست", "error", "تلاش مجدد ");
+    }
+
+    const isValidEmail = validateEmail(data.email);
+    if (!isValidEmail) {
+      return showSwal("ایمیل وارد شده معتبر نیست", "error", "تلاش مجدد ");
+    }
+
+    const isValidPassword = validatePassword(data.password);
+    if (!isValidPassword) {
+      return showSwal(
+        "لطفا رمز عبور امن تری را وارد کنید !",
+        "error",
+        "تلاش مجدد "
+      );
+    }
+
+    const res = await fetch("api/auth/register", {
       method: "POST",
       headers: {
         "content-Type": "application/json",
       },
       body: JSON.stringify(data),
-    })
-      .then((res) => {
-        if (res.status === 201) {
-          console.log("ok = ", res);
-          reset();
-        } else if (res.status === 422) {
-        } else if (res.status === 404) {
-        } else if (res.status === 500) {
-        }
-        return res.json();
-      })
-      .catch((err) => console.log("can not sent message", err));
+    });
+    if (res.status === 201) {
+      showSwal("ثبت نام با موفقیت انجام شد", "success", "ورود به پنل کاربری");
+      reset();
+    } else if (res.status === 422) {
+      showSwal("کاربری با این اطلاعات از قبل وجود دارد", "error", "تلاش مجدد");
+    }
     console.log("data =", data);
   };
+  console.log("isDirty", isDirty);
+  console.log("isValid", isValid);
+
   return (
     <form
       className="p-6 space-y-4 md:space-y-6 sm:p-8"
       action="#"
       onSubmit={handleSubmit(onSubmit)}
     >
-      <div>
-        <label
-          htmlFor="fullName"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          نام و نام خانوادگی
-        </label>
-        <input
-          type="fullName"
-          id="fullName"
-          placeholder="  نام و نام خانوادگی"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          {...register("fullName", { required: true })}
-        />
-        {errors.fullName && <div className="text-pink-700">...</div>}
-      </div>
-
       <div>
         <label
           htmlFor="userName"
@@ -67,7 +93,6 @@ export default function Register() {
           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           {...register("userName", { required: true })}
         />
-        {errors.userName && <div className="text-pink-700">...</div>}
       </div>
 
       <div>
@@ -84,7 +109,6 @@ export default function Register() {
           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           {...register("email", { required: true })}
         />
-        {errors.email && <div className="text-pink-700">...</div>}
       </div>
 
       <div>
@@ -101,7 +125,6 @@ export default function Register() {
           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           {...register("phoneNumber", { required: true })}
         />
-        {errors.phoneNumber && <div className="text-pink-700">...</div>}
       </div>
 
       <div>
@@ -118,20 +141,21 @@ export default function Register() {
           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           {...register("password", { required: true })}
         />
-        {errors.password && <div className="text-pink-700">...</div>}
       </div>
 
       <button
         type="submit"
         className="w-full text-dark bg-zinc-300 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+        disabled={!isDirty || !isValid}
       >
         ثبت نام
       </button>
       <p className="text-sm text-end font-light text-gray-500 dark:text-gray-400">
         حساب کاربری دارید؟
         <Link
-          href="/login"
+          href="/login-register"
           className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+          onClick={showloginForm}
         >
           لاگین
         </Link>
