9e0b3aec1e4a31272c1e4a148498f40e3c2a7be8
marzieh <marziehmonshizade@yahoo.com>
2024-05-02 00:49:17 +0330

    add : coding validation datas functions with regex

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -36,10 +36,33 @@ const verifyAccessToken = (token) => {
   }
 };
 
+// validation data with regex
+const validateUserName = (userName) => {
+  const pattern = /^[a-z0-9_-]{3,15}$/g;
+  return pattern.test(userName);
+};
+const validateEmail = (email) => {
+  const pattern = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/g;
+  return pattern.test(email);
+};
+const validatePhone = (phone) => {
+  const pattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/g;
+  return pattern.test(phone);
+};
+const validatePassword = (password) => {
+  const pattern =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/g;
+  return pattern.test(password);
+};
+
 export {
   generateAccessToken,
   generateRefreshToken,
   hashPassword,
+  validateEmail,
+  validatePassword,
+  validatePhone,
+  validateUserName,
   verifyAccessToken,
   verifyPassword,
 };
