7050e21d120d073da40501f42916208d8c95bee8
marzieh <marziehmonshizade@yahoo.com>
2024-05-05 19:45:54 +0330

    add page: coding training page

---
+++ src/app/weblog/training/page.tsx
@@ -0,0 +1,96 @@
+import WeblogArticle from "@/components/templates/weblog/article";
+import WeblogHeader from "@/components/templates/weblog/header";
+
+export default function Training() {
+  return (
+    <>
+      <WeblogHeader title="آموزش" />
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4 my-10 lg:mx-12 md:mx-2 mx-0">
+        <WeblogArticle
+          imgUrl="/images/articles/TurkCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/TurkCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/TurkCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/TurkCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/TurkCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/TurkCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/TurkCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/TurkCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/TurkCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/TurkCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/TurkCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/TurkCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+      </div>
+    </>
+  );
+}
