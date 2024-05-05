6365638fe69befbebaf6524700ece9ebdfc987fc
marzieh <marziehmonshizade@yahoo.com>
2024-05-05 14:26:06 +0330

    add : coding service

---
+++ src/components/templates/index/ourServices/service.tsx
@@ -0,0 +1,17 @@
+type ServiceProps = {
+  text: string;
+  children: React.ReactNode;
+};
+
+function Service({ text, children }: ServiceProps) {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="bg-[#2e2b27] dark:bg-[#D1B48C] w-32 h-32 p-8 rounded-full">
+        {children}
+      </div>
+      <p className="text-[#2e2b27] dark:text-[#D1B48C] py-2">{text}</p>
+    </div>
+  );
+}
+
+export default Service;
