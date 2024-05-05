ab7f8612a81aad2883ecd5acd0645501a33f0a48
marzieh <marziehmonshizade@yahoo.com>
2024-05-05 19:46:09 +0330

    add page: coding news page

---
+++ src/app/weblog/news/page.tsx
@@ -0,0 +1,98 @@
+import WeblogArticle from "@/components/templates/weblog/article";
+import WeblogHeader from "@/components/templates/weblog/header";
+
+function News() {
+  return (
+    <>
+      <WeblogHeader title="اخبار و مقالات" />
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4 my-10 lg:mx-12 md:mx-2 mx-0">
+        <WeblogArticle
+          imgUrl="/images/articles/AboutCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/AboutCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/AboutCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/AboutCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/AboutCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/AboutCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/AboutCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/AboutCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/AboutCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/AboutCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/AboutCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+        <WeblogArticle
+          imgUrl="/images/articles/AboutCoffee.jpg"
+          title="مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها"
+          des="  مشکلات رایج دستگاه قهوه ساز و عیب یابی آن ها امروزه یکی از لوازم خانگی
+        پرکاربرد در آشپزخانه ها دستگاه"
+          link="#"
+        />
+      </div>
+    </>
+  );
+}
+
+export default News;
