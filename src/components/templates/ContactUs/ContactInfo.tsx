1283f946c09f9184fbae782d6207b65ac5c2bd97
marzieh <marziehmonshizade@yahoo.com>
2024-05-07 16:23:12 +0330

    add: create contact information template

---
+++ src/components/templates/ContactUs/ContactInfo.tsx
@@ -0,0 +1,22 @@
+function ContactInfo() {
+  return (
+    <div className="text-slate-500 dark:text-white leading-10 max-w-[500px]">
+      <div className="text-slate-400">تماس با ما</div>
+      <h2 className="text-2xl font-extrabold text-black dark:text-white mb-4">
+        اطلاعات تماس
+      </h2>
+      <div>شرکت فنجان داغ خوارزمی (کارخانه قهوه ست )</div>
+      <div>set-coffee.com</div>
+      <div>
+        تهران. پاکدشت . شهرک صنعتی خوارزمی. فاز 2 . بلوار بهارستان. خیابان
+        ماگنولیا بلوک آ117
+      </div>
+      <div>021-36479228</div>
+      <div>coffee[at]set-coffee.com</div>
+      <div>whole[at]set-coffee.com</div>
+      <div>تماس با مدیریت از طریق واتساپ و یا تلگرام : 09366726563</div>
+    </div>
+  );
+}
+
+export default ContactInfo;
