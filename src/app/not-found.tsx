0d0ed8b359f845f3d0417c1197e9d8a169d022de
marzieh <marziehmonshizade@yahoo.com>
2024-05-09 15:50:50 +0330

    add: coding not found  page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function notFound() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-2 bg-orange-50 w-full h-screen">
+      <Image
+        className="w-44 h-44"
+        alt="notFound"
+        src="/images/404-icon.png"
+        width={200}
+        height={200}
+      />
+      <p className="font-semibold">صفحه ی مورد نظر متاسفانه یافت نشد!!!</p>
+      <Link href="/" className="font-extrabold text-[#F4A518] hover:underline">
+        صفحه ی اصلی
+      </Link>
+    </div>
+  );
+}
+
+export default notFound;
