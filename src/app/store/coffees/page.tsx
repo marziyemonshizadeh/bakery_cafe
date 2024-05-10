70d5967533e396a51e3f3798a38c46fa3a7141e6
marzieh <marziehmonshizade@yahoo.com>
2024-05-10 20:30:24 +0330

    new page: coding coffees page

---
+++ src/app/store/coffees/page.tsx
@@ -0,0 +1,56 @@
+import Product from "@/components/modules/product/product";
+import Header from "@/components/templates/store/header";
+
+type Props = {};
+
+function Coffees({}: Props) {
+  return (
+    <div className="overflow-hidden">
+      <Header imgUrl="images/Coffee/coffeeBG.jpg" />
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 my-8">
+        <Product
+          imgUrl="images/Coffee/alibaba.png"
+          title="دانه قهوه عربیکا"
+          price={2000000}
+        />
+        <Product
+          imgUrl="images/Coffee/setCoffee.png"
+          title="دانه قهوه عربیکا"
+          price={2000000}
+        />
+        <Product
+          imgUrl="images/Coffee/jacobs_espresso.png"
+          title="دانه قهوه عربیکا"
+          price={2000000}
+        />
+        <Product
+          imgUrl="images/Coffee/jacobs-mokka.png"
+          title="دانه قهوه عربیکا"
+          price={2000000}
+        />
+        <Product
+          imgUrl="images/Coffee/jacobs.webp"
+          title="دانه قهوه عربیکا"
+          price={2000000}
+        />
+        <Product
+          imgUrl="images/Coffee/nescafeGold.jpg"
+          title="دانه قهوه عربیکا"
+          price={2000000}
+        />
+        <Product
+          imgUrl="images/Coffee/nescoffee.png"
+          title="دانه قهوه عربیکا"
+          price={2000000}
+        />
+        <Product
+          imgUrl="images/Coffee/turk-kahvesi.png"
+          title="دانه قهوه عربیکا"
+          price={2000000}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Coffees;
