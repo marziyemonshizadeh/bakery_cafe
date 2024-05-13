eb5b9c0f1c33e080168dde011bf223586cc2b391
marzieh <marziehmonshizade@yahoo.com>
2024-05-13 20:57:04 +0330

    feature: Show userName in the navbar when logging in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,24 @@ import Header from "@/components/templates/index/header/header";
 import LatestBread from "@/components/templates/index/latestBread/latestBread";
 import LatestCoffee from "@/components/templates/index/latestCoffee/latestCoffee";
 import LatestPastry from "@/components/templates/index/latestPastry/latestPastry";
+import OurServices from "@/components/templates/index/ourServices/ourServices";
 import Promote from "@/components/templates/index/promote/promote";
+import SecondPromote from "@/components/templates/index/secondPromote/secondPromote";
+import { authUser } from "@/utils/auth";
 
-export default function Home() {
+export default async function Home() {
+  const user = await authUser();
+
   return (
-    <main className="bg-white dark:bg-[#2e2b27]">
-      <Navbar />
+    <main className="bg-white dark:bg-[#2e2b27] relative">
+      <Navbar isLogin={user ? true : false} userName={user?.userName} />
       <Header />
       <LatestCoffee />
       <LatestBread />
       <LatestPastry />
       <Promote />
+      <OurServices />
+      <SecondPromote />
       <Articles />
       <Footer />
     </main>
