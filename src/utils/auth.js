c09706c9c52af592be8ba60292a0a2baf10f219f
marzieh <marziehmonshizade@yahoo.com>
2024-05-13 20:25:28 +0330

    add: add authUser function

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,9 @@
+"use server";
+import UserModel from "@/models/user";
 import { compare, hash } from "bcryptjs";
 import { sign, verify } from "jsonwebtoken";
+import { cookies } from "next/headers";
+import connectToDB from "./db";
 
 const hashPassword = async (password) => {
   const hashedPassword = await hash(password, 12);
@@ -8,7 +12,8 @@ const hashPassword = async (password) => {
 
 const generateAccessToken = (data) => {
   const token = sign({ ...data }, process.env.ACCESS_TOKEN_SECRET_KEY, {
-    expiresIn: "60s",
+    // expiresIn: "60s",
+    expiresIn: "7d",
   });
   return token;
 };
@@ -55,7 +60,29 @@ const validatePassword = (password) => {
   return pattern.test(password);
 };
 
+const authUser = async () => {
+  connectToDB();
+  const token = cookies().get("token");
+  console.log("token ===", token);
+  let user = null;
+  // const auth = useContext(AuthContext);
+
+  if (token) {
+    const tokenPayload = verifyAccessToken(token.value);
+    console.log("tokenPayload=", tokenPayload);
+    if (tokenPayload) {
+      // auth?.setUser({
+      //   email: tokenPayload.email,
+      // });
+      user = await UserModel.findOne({ email: tokenPayload.email });
+    }
+  }
+
+  return user;
+};
+
 export {
+  authUser,
   generateAccessToken,
   generateRefreshToken,
   hashPassword,
