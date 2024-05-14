f6363d949b426fb2526c847d189f3261089c8459
marzieh <marziehmonshizade@yahoo.com>
2024-05-14 12:40:15 +0330

    fix error: fixed register api error

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -3,23 +3,20 @@ import { generateAccessToken, hashPassword } from "@/utils/auth";
 import { roles } from "@/utils/constants";
 import connectToDB from '@/utils/db';
 import { showSwal } from "@/utils/helpers";
-// import usersValidator from "@/validators/user";
+import usersValidator from "@/validators/user";
 
 
 export async function POST(request: Request) {
+  
+  try{   
     connectToDB()
-    // const validationResult = usersValidator(request.body)   
-    // console.log("validationResult===",validationResult);
-       
-    // if(validationResult !== true){      
-    //   return Response.json({ message: "validation data and found your data is not valid !!" },{status:422});
-    // }
-    try{
-
-      const body = await request.json()
-      const { userName, email, phoneNumber, password } = body;
-      
-      // validation
+    const body = await request.json()
+    const { userName, email, phoneNumber, password } = body;
+    
+    const validationResult = usersValidator({ userName, email, phoneNumber, password })   
+    if(validationResult !== true){      
+          return Response.json({ message: "validation data and found your data is not valid !!" },{status:422});
+    }
   
       if(!userName.trim()){
          return showSwal(
@@ -59,7 +56,7 @@ export async function POST(request: Request) {
         password: hashedPassword,
         role: users.length > 0 ? roles.USER : roles.ADMIN,
       });
-      return Response.json({ message: "send get request " },{status:201 , headers:{"Set-Cookie":`token = ${accessToken};path= "/";httpOnly= true`}});
+      return Response.json({ message: "User register Successfully :))" },{status:201 , headers:{"Set-Cookie":`token=${accessToken};path=/;httpOnly=true`}});
     }catch (err) {
       return Response.json({ message: "register has UnKnown internal server error !!" },{status:500 });
 
