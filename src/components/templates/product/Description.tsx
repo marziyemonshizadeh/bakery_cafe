41460ed12651ca81610a9a56c94e5ab2efecaeb4
marzieh <marziehmonshizade@yahoo.com>
2024-05-19 13:48:56 +0330

    add template: create description template for product page

---
+++ src/components/templates/product/Description.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+function Descriptions({ longDescription }: any) {
+  return (
+    <div className="mb-40">
+      <h2 className="text-2xl font-extrabold my-2">
+        دانه قهوه کلمبیا از منطقه Nariño
+      </h2>
+      <ul className="text-slate-600 dark:text-slate-300">
+        <li>Region: Nariño</li>
+        <li>Variety: CASTILLO , CATURRA</li>
+        <li>Farm owner: Various Local farmers</li>
+        <li>Altitude: 1780 masl</li>
+        <li>Notes:Citrus fruit, Caramel, Vanilla</li>
+      </ul>
+      <h2 className="text-2xl font-extrabold my-2">بررسی محصول</h2>
+      <Image
+        className="mx-auto h-auto max-w-md m-4"
+        alt="cafe_de_colombia"
+        src="/images/cafe_de_colombia.jpg"
+        width={200}
+        height={200}
+      />
+      <p className="text-slate-600 dark:text-slate-300">{longDescription}</p>
+    </div>
+  );
+}
+
+export default Descriptions;
