19b3c8ff53f06f172a629684c7939e3b2370f48b
marzieh <marziehmonshizade@yahoo.com>
2024-05-20 14:41:10 +0330

    add type: add product form value type

--- src/types/typings.d.ts
+++ src/types/typings.d.ts
@@ -1,31 +1,37 @@
-export interface loginFormValues{
-    identifier:string|number,
-    password:string,
-    remember:boolean
+export interface loginFormValues {
+  identifier: string | number;
+  password: string;
+  remember: boolean;
 }
-export interface registerFormValues{
-    userName:string,
-    password:string,
-    email:string,
-    phoneNumber:number,
+export interface registerFormValues {
+  userName: string;
+  password: string;
+  email: string;
+  phoneNumber: number;
 }
-export interface loginByPhoneNumberFormValues{
-    phoneNumber:number,
+export interface loginByPhoneNumberFormValues {
+  phoneNumber: number;
 }
-export interface verifyCodeFormValues{
-    code:number
+export interface verifyCodeFormValues {
+  code: number;
 }
-export interface forgetPasswordFormValues{
-    identifier:number|string,
+export interface forgetPasswordFormValues {
+  identifier: number | string;
 }
-export interface setNewPasswordFormValues{
-    code:number,
-    password:string,
+export interface setNewPasswordFormValues {
+  code: number;
+  password: string;
 }
-export interface contactFormValues{
-    email:string,
-    fullName:string,
-    phoneNumber:string,
-    companyName:string,
-    message:string,
+export interface contactFormValues {
+  email: string;
+  fullName: string;
+  phoneNumber: string;
+  companyName: string;
+  message: string;
+}
+export interface productFormValues {
+  email: string;
+  name: string;
+  score: number;
+  message: string;
 }
