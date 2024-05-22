f7ec6cf176b31a06e69406b7f6a4a90d5d52c27c
marzieh <marziehmonshizade@yahoo.com>
2024-05-22 23:49:52 +0330

    add feature: coding just show isAccept comments

--- src/components/templates/product/comments.tsx
+++ src/components/templates/product/comments.tsx
@@ -5,12 +5,16 @@ type CommentsProps = { comments: any; productID: number };
 
 function Comments({ comments, productID }: CommentsProps) {
   return (
-    <div className="flex lg:flex-row flex-col  lg:justify-between justify-center gap-4 my-8">
-      <div className="min-w-[600px]">
-        <p className="font-bold">{comments.length} دیدگاه برای قهوه اسپرسو </p>
-        {comments?.map((comment: any) => (
-          <Comment key={comment._id} {...comment} />
-        ))}
+    <div className="flex lg:flex-row flex-col  lg:justify-evenly justify-center gap-4 my-8 overflow-hidden">
+      <div>
+        <p className="font-bold">
+          {comments.filter((comment: any) => comment.isAccept).length} دیدگاه
+          برای قهوه اسپرسو{" "}
+        </p>
+        {comments?.map(
+          (comment: any) =>
+            comment.isAccept && <Comment key={comment._id} {...comment} />
+        )}
       </div>
       <CommentForm productID={productID} />
     </div>
