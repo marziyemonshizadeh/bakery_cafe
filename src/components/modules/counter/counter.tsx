bc9d06ba17667754f6cacdb0c7d01c929225bd28
marzieh <marziehmonshizade@yahoo.com>
2024-05-23 19:44:18 +0330

    add module: coding counter module

---
+++ src/components/modules/counter/counter.tsx
@@ -0,0 +1,60 @@
+"use client";
+import Link from "next/link";
+import { useReducer } from "react";
+import { ActionTypes, CounterType } from "../product/product.type";
+
+type Props = {};
+
+const initialState = {
+  count: 0,
+};
+
+const reducer = (state: CounterType, action: ActionTypes) => {
+  switch (action.type) {
+    case "increment":
+      if (state.count == 5) {
+        return { count: state.count + 0 };
+      }
+      return { count: state.count + action.payload };
+    case "decrement":
+      if (state.count == 0) {
+        return { count: state.count };
+      }
+      return { count: state.count - action.payload };
+    default: {
+      throw new Error("Invalid Action Type !!");
+    }
+  }
+};
+
+function Counter({}: Props) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <Link
+        href="#"
+        className="bg-[#413a2d] text-[#D1B48C] border border-[#D1B48C] dark:bg-[#D1B48C] dark:text-[#413a2d] shadow shadow-[#D1B48C] font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:bg-[#4d4435] focus:ring-1 focus:outline-none focus:ring-[#eacfaa]  sm:w-auto dark:hover:bg-[#f6dcb8]"
+        onClick={(e) => {
+          e.preventDefault();
+          dispatch({ type: "increment", payload: 1 });
+        }}
+      >
+        +
+      </Link>
+      <span> {state.count}</span>
+      <Link
+        href="#"
+        className="bg-[#413a2d] text-[#D1B48C] border border-[#D1B48C] dark:bg-[#D1B48C] dark:text-[#413a2d] shadow shadow-[#D1B48C] font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:bg-[#4d4435] focus:ring-1 focus:outline-none focus:ring-[#eacfaa]  sm:w-auto dark:hover:bg-[#f6dcb8]"
+        onClick={(e) => {
+          e.preventDefault();
+          dispatch({ type: "decrement", payload: 1 });
+        }}
+      >
+        -
+      </Link>
+    </>
+  );
+}
+
+export default Counter;
