9dbc42ef1b5814f7759d953cbed3d3ea096651e8
marzieh <marziehmonshizade@yahoo.com>
2024-05-23 23:56:36 +0330

    refactor: get real pastries datas from database

--- src/app/store/pastries/page.tsx
+++ src/app/store/pastries/page.tsx
@@ -1,109 +1,24 @@
 import Product from "@/components/modules/product/product";
 import Header from "@/components/templates/store/header";
+import productModel from "@/models/product";
+import connectToDB from "@/utils/db";
 
-type Props = {};
+async function Pastries() {
+  connectToDB();
+  const pastries = await productModel.find({ category: "شیرینی" });
 
-function Pastries({}: Props) {
   return (
     <div className="overflow-hidden">
       <Header imgUrl="images/Pastry/pastriyBG.jpg" />
-      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 my-8">
-        <Product
-          imgUrl="images/Pastry/Chocolate.png"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
-        <Product
-          imgUrl="images/Pastry/ApplePie.png"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
-        <Product
-          imgUrl="images/Pastry/CROISSANT_BOYTYROY.png"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
-        <Product
-          imgUrl="images/Pastry/CROISSANT_PRALINA.png"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
-        <Product
-          imgUrl="images/Pastry/Eclair_1.png"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
-        <Product
-          imgUrl="images/Pastry/egg-tart-with-ai-generated-free-png.webp"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
-        <Product
-          imgUrl="images/Pastry/ir.ashpazifu.fu_512x512.webp"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
-        <Product
-          imgUrl="images/Pastry/khamei.png"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
-        <Product
-          imgUrl="images/Pastry/lemon tart.png"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
-        <Product
-          imgUrl="images/Pastry/macaroone.webp"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
-        <Product
-          imgUrl="images/Pastry/macaroons.png"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
-        <Product
-          imgUrl="images/Pastry/NY_cheesecake1.png"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
-        <Product
-          imgUrl="images/Pastry/PARIS-BREST.png"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
-        <Product
-          imgUrl="images/Pastry/tiramisu cup.png"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
-        <Product
-          imgUrl="images/Pastry/trilice.png"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
-        <Product
-          imgUrl="images/Pastry/VanillaCremeBruleeRamekin.png"
-          title="دانه قهوه عربیکا"
-          price={2000000}
-          score= {5}
-        />
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 my-8 mx-10">
+        {pastries.map((pastry: any) => (
+          <Product
+            imgUrl={`images/${pastry.image}`}
+            title={pastry.name}
+            price={pastry.price}
+            score={pastry.score}
+          />
+        ))}
       </div>
     </div>
   );
