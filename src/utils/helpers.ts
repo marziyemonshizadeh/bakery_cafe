c8ba8ec8a1b5eb6b90fabb4427c0ce3616483788
marzieh <marziehmonshizade@yahoo.com>
2024-05-24 14:19:09 +0330

    refactor: refactor show swal function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,12 +1,14 @@
 const swal = require("sweetalert");
 
-const showSwal = (title:string, icon:string, buttons:string, url:Location | (string & Location) | any) => {
+const showSwal = (
+  title: string,
+  icon: string,
+  buttons: string,
+  url: Location | (string & Location) | any
+) => {
   swal({ title, icon, buttons }).then(function () {
     window.location = url;
-    // router.replace(url);
-
   });
 };
 
 export { showSwal };
-
