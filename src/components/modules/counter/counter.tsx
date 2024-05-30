30d76254b8a88a3fc0c0ed60d498aa1903b75086
marzieh <marziehmonshizade@yahoo.com>
2024-05-30 17:17:21 +0330

    style: Add some classes

--- src/components/modules/counter/counter.tsx
+++ src/components/modules/counter/counter.tsx
@@ -31,10 +31,10 @@ function Counter({}: Props) {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <>
+    <div>
       <Link
         href="#"
-        className="bg-[#413a2d] text-[#D1B48C] border border-[#D1B48C] dark:bg-[#D1B48C] dark:text-[#413a2d] shadow shadow-[#D1B48C] font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:bg-[#4d4435] focus:ring-1 focus:outline-none focus:ring-[#eacfaa]  sm:w-auto dark:hover:bg-[#f6dcb8]"
+        className="bg-[#413a2d] text-[#D1B48C] border border-[#D1B48C] dark:bg-[#D1B48C] dark:text-[#413a2d] shadow shadow-[#D1B48C] font-medium rounded-lg text-sm px-3 py-1.5 text-center hover:bg-[#4d4435] focus:ring-1 focus:outline-none focus:ring-[#eacfaa]  sm:w-auto dark:hover:bg-[#f6dcb8]"
         onClick={(e) => {
           e.preventDefault();
           dispatch({ type: "increment", payload: 1 });
@@ -42,10 +42,10 @@ function Counter({}: Props) {
       >
         +
       </Link>
-      <span> {state.count}</span>
+      <span className="mx-1"> {state.count}</span>
       <Link
         href="#"
-        className="bg-[#413a2d] text-[#D1B48C] border border-[#D1B48C] dark:bg-[#D1B48C] dark:text-[#413a2d] shadow shadow-[#D1B48C] font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:bg-[#4d4435] focus:ring-1 focus:outline-none focus:ring-[#eacfaa]  sm:w-auto dark:hover:bg-[#f6dcb8]"
+        className="bg-[#413a2d] text-[#D1B48C] border border-[#D1B48C] dark:bg-[#D1B48C] dark:text-[#413a2d] shadow shadow-[#D1B48C] font-medium rounded-lg text-sm px-3 py-1.5 text-center hover:bg-[#4d4435] focus:ring-1 focus:outline-none focus:ring-[#eacfaa]  sm:w-auto dark:hover:bg-[#f6dcb8]"
         onClick={(e) => {
           e.preventDefault();
           dispatch({ type: "decrement", payload: 1 });
@@ -53,7 +53,7 @@ function Counter({}: Props) {
       >
         -
       </Link>
-    </>
+    </div>
   );
 }
 
--- src/components/templates/weblog/header.tsx
+++ src/components/templates/weblog/header.tsx
@@ -4,9 +4,9 @@ type HeaderProps = { title: string };
 
 function WeblogHeader({ title }: HeaderProps) {
   return (
-    <header className="relative w-full h-96 bg-orange-950 text-white flex flex-col justify-center items-center gap-3">
-      <h2 className="font-bold text-3xl"> {title}</h2>
-      <div className="block">
+    <header className="relative w-full h-96 dark:bg-[#BAA799] bg-[#9b8e7b] text-white flex flex-col justify-center items-center gap-3">
+      <h2 className="font-bold text-3xl dark:text-[#413a2d]"> {title}</h2>
+      <div className="block dark:text-[#413a2d]">
         <Link href="/" className="hover:underline">
           خانه
         </Link>
