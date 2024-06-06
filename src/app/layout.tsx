db28c715b0c2a84eea62383843e312b487c6232f
marzieh <marziehmonshizade@yahoo.com>
2024-06-06 16:44:21 +0330

    delete extra comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
-// "use client";
 import ScrollToTop from "@/components/modules/scrollToTop/scrollToTop";
-// import { AuthContextProvider } from "@/context/auth/authContext";
 import AosInit from "@/utils/aos";
 import ThemeProv from "@/utils/theme";
+import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
 
 export const metadata = {
   title: "Bakery_Cafe",
   description: "coffee / bakary project with next.js v13",
   icons: {
-    // https://seeklogo.com/images/C/coffee-time-logo-187E7F85DE-seeklogo.com.png
     icon: "https://seeklogo.com/images/C/coffee-time-logo-187E7F85DE-seeklogo.com.png",
   },
 };
@@ -23,14 +21,11 @@ export default function RootLayout({
     <html lang="fa" dir="rtl" suppressHydrationWarning={true}>
       <body>
         <ThemeProv>
-          {/* <AuthContextProvider> */}
-          {/* scrollbar-thin h-screen  scrollbar-thumb-[#413a2d] dark:scrollbar-thumb-[#D1B48C] scrollbar-track-[#D1B48C] dark:scrollbar-track-[#413a2d]  overflow-y-scroll */}
           <div className="static">
             <AosInit />
             {children}
             <ScrollToTop />
           </div>
-          {/* </AuthContextProvider> */}
         </ThemeProv>
       </body>
     </html>
