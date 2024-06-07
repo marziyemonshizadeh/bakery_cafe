f1ff6c7e5ca72e51a4ec1bac11cbd73840993f24
marzieh <marziehmonshizade@yahoo.com>
2024-06-07 23:05:17 +0330

    refactor: find real comments and then transforms it into CommentsTable component

--- src/app/p-user/comments/page.tsx
+++ src/app/p-user/comments/page.tsx
@@ -1,24 +1,30 @@
 import Layout from "@/components/layouts/panelUserLayout";
+import Title from "@/components/modules/panelUser/panelUserTitle";
 import CommentsTable from "@/components/templates/p-user/comments/commentsTable";
 import ThereIsNoComment from "@/components/templates/p-user/comments/thereIsNoComment";
-// import CommentModel from "@/models/comment";
-// import connectToDB from "@/utils/db";
-// import { authUser } from "@/utils/serverHelpers";
+import CommentModel from "@/models/comment";
+import connectToDB from "@/utils/db";
+import { authUser } from "@/utils/serverHelpers";
 
 async function Comments() {
-  // connectToDB();
-  // const user = await authUser();
-  // let comments = [];
-  // if (user) {
-  //   comments = await CommentModel.find({ userID: user._id }, "-__v");
-  // }
+  connectToDB();
+  const user = await authUser();
+  let comments = [];
+  if (user) {
+    comments = await CommentModel.find({ userID: user._id }, "-__v").populate(
+      "productID",
+      "name"
+    );
+    console.log("user panel comments", comments);
+  }
   return (
     <Layout>
-      <h2 className="font-bold text-2xl border-b border-x-amber-800 py-4">
-        کامنت های ثبت شده
-      </h2>
-      <CommentsTable />
-      <ThereIsNoComment />
+      <Title text="کامنت های ثبت شده" />
+      {comments.length > 0 ? (
+        <CommentsTable comments={JSON.parse(JSON.stringify(comments))} />
+      ) : (
+        <ThereIsNoComment />
+      )}
     </Layout>
   );
 }
