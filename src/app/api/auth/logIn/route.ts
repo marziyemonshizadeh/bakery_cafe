469cc715d6dbe0b5d7dd97aa945220448f684837
marzieh <marziehmonshizade@yahoo.com>
2024-06-07 21:51:13 +0330

    refactor auth api

--- src/app/api/auth/logIn/route.ts
+++ src/app/api/auth/logIn/route.ts
@@ -1,61 +1,84 @@
 import UserModel from "@/models/user";
-import { generateAccessToken, generateRefreshToken, validatePassword, verifyPassword } from "@/utils/auth";
-import connectToDB from '@/utils/db';
+import {
+  generateAccessToken,
+  generateRefreshToken,
+  validatePassword,
+  verifyPassword,
+} from "@/utils/auth";
+import connectToDB from "@/utils/db";
 
- 
 export function GET() {
-    console.log("get request");
-    return Response.json({ message: "send get req " });
-
+  console.log("get request");
+  return Response.json({ message: "send get req " });
 }
 export async function POST(request: Request) {
-  try{
-      connectToDB()
+  try {
+    connectToDB();
 
-        const body = await request.json()
-        const { identifier, password } = body;
-        
-    
-        if (
-            !identifier.trim() ||
-            !password.trim()) {
-            return Response.json({ message: "Data is not valid !!" },{status:419});
-          }
-        
-          const ValidationPassword = validatePassword(password);
-          if (!ValidationPassword) {
-            return Response.json({ message: "password is not valid !!" },{status:422});
-          }
-    
-        const isUserExist = await UserModel.findOne({
-            $or: [{ phoneNumber: identifier }, { email: identifier }],
-          });
-        
-        if (!isUserExist) {
-           return Response.json({ message: "email or phone or pass not found !!" },{status:422});
-        } 
-        
-        const isMatchPasswordWithHash = await verifyPassword(password, isUserExist.password); 
-        if (!isMatchPasswordWithHash) {
-            return Response.json({ message: "your mail or password is not correct !!" },{status:401});
-          }
-        
-        const accessToken = generateAccessToken({ email :identifier});
-        console.log("accessToken",accessToken);
-        
-        const refreshToken = generateRefreshToken({ email :identifier});
-        console.log("refreshToken",refreshToken);
-        
-        
-        await UserModel.findOneAndUpdate({email:isUserExist.email },{$set:{refreshToken}})
-        return Response.json({ message: "User Logged In Successfully :))" },{status:200 , headers:{"Set-Cookie":`token=${accessToken};path=/;httpOnly=true`}}); 
-      }catch (err) {
-        console.log("log in has UnKnown internal server error ->", err);
-        return Response.json(
-          { message: err },
-          {
-            status: 500,
-          }
-        );  
+    const body = await request.json();
+    const { identifier, password } = body;
+
+    if (!identifier.trim() || !password.trim()) {
+      return Response.json(
+        { message: "Data is not valid !!" },
+        { status: 419 }
+      );
+    }
+
+    const ValidationPassword = validatePassword(password);
+    if (!ValidationPassword) {
+      return Response.json(
+        { message: "password is not valid !!" },
+        { status: 422 }
+      );
+    }
+
+    const isUserExist = await UserModel.findOne({
+      $or: [{ phoneNumber: identifier }, { email: identifier }],
+    });
+
+    if (!isUserExist) {
+      return Response.json(
+        { message: "email or phone or pass not found !!" },
+        { status: 422 }
+      );
+    }
+
+    const isMatchPasswordWithHash = await verifyPassword(
+      password,
+      isUserExist.password
+    );
+    if (!isMatchPasswordWithHash) {
+      return Response.json(
+        { message: "your mail or password is not correct !!" },
+        { status: 401 }
+      );
+    }
+
+    const accessToken = generateAccessToken({ email: identifier });
+    console.log("accessToken", accessToken);
+
+    const refreshToken = generateRefreshToken({ email: identifier });
+    console.log("refreshToken", refreshToken);
+
+    await UserModel.findOneAndUpdate(
+      { email: isUserExist.email },
+      { $set: { refreshToken } }
+    );
+    return Response.json(
+      { message: "User Logged In Successfully :))" },
+      {
+        status: 200,
+        headers: { "Set-Cookie": `token=${accessToken};path=/;httpOnly=true` },
       }
+    );
+  } catch (err) {
+    console.log("log in has UnKnown internal server error ->", err);
+    return Response.json(
+      { message: err },
+      {
+        status: 500,
+      }
+    );
+  }
 }
--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,64 +1,84 @@
 import UserModel from "@/models/user";
 import { generateAccessToken, hashPassword } from "@/utils/auth";
 import { roles } from "@/utils/constants";
-import connectToDB from '@/utils/db';
+import connectToDB from "@/utils/db";
 import { showSwal } from "@/utils/helpers";
 import usersValidator from "@/validators/user";
 
-
 export async function POST(request: Request) {
-  
-  try{   
-    connectToDB()
-    const body = await request.json()
+  try {
+    connectToDB();
+    const body = await request.json();
     const { userName, email, phoneNumber, password } = body;
-    
-    const validationResult = usersValidator({ userName, email, phoneNumber, password })   
-    if(validationResult !== true){      
-          return Response.json({ message: "validation data and found your data is not valid !!" },{status:422});
-    }
-  
-      if(!userName.trim()){
-         return showSwal(
-              "نام کاربری اجباری می باشد لطفا وارد کنید",
-              "error",
-              "تلاش مجدد","#"
-            );
-      }
-  
-      if (
-          !userName.trim() ||
-          !email.trim()||
-          !phoneNumber.trim()||
-          !password.trim()) {
-          return Response.json({ message: "Data is not valid !!" },{status:422});
-        }
-  
-  
-      const isUserExist = await UserModel.findOne({
-          $or: [{ userName }, { email }, {phoneNumber}],
-        });
-      
-      if (isUserExist) {
-         return Response.json({ message: "This username or email or phoneNumber exist already !!" },{status:422});
-      } 
-  
-      const hashedPassword = await hashPassword(password); 
-      const accessToken = generateAccessToken({ email });
-      
-  
-      const users = await UserModel.find({});
-      
-      await UserModel.create({
-        userName,
-        email,
-        phoneNumber,
-        password: hashedPassword,
-        role: users.length > 0 ? roles.USER : roles.ADMIN,
-      });
-      return Response.json({ message: "User register Successfully :))" },{status:201 , headers:{"Set-Cookie":`token=${accessToken};path=/;httpOnly=true`}});
-    }catch (err) {
-      return Response.json({ message: "register has UnKnown internal server error !!" },{status:500 });
 
+    const validationResult = usersValidator({
+      userName,
+      email,
+      phoneNumber,
+      password,
+    });
+    if (validationResult !== true) {
+      return Response.json(
+        { message: "validation data and found your data is not valid !!" },
+        { status: 422 }
+      );
     }
+
+    if (!userName.trim()) {
+      return showSwal(
+        "نام کاربری اجباری می باشد لطفا وارد کنید",
+        "error",
+        "تلاش مجدد",
+        "#"
+      );
+    }
+
+    if (
+      !userName.trim() ||
+      !email.trim() ||
+      !phoneNumber.trim() ||
+      !password.trim()
+    ) {
+      return Response.json(
+        { message: "Data is not valid !!" },
+        { status: 422 }
+      );
+    }
+
+    const isUserExist = await UserModel.findOne({
+      $or: [{ userName }, { email }, { phoneNumber }],
+    });
+
+    if (isUserExist) {
+      return Response.json(
+        { message: "This username or email or phoneNumber exist already !!" },
+        { status: 422 }
+      );
+    }
+
+    const hashedPassword = await hashPassword(password);
+    const accessToken = generateAccessToken({ email });
+
+    const users = await UserModel.find({});
+
+    await UserModel.create({
+      userName,
+      email,
+      phoneNumber,
+      password: hashedPassword,
+      role: users.length > 0 ? roles.USER : roles.ADMIN,
+    });
+    return Response.json(
+      { message: "User register Successfully :))" },
+      {
+        status: 201,
+        headers: { "Set-Cookie": `token=${accessToken};path=/;httpOnly=true` },
+      }
+    );
+  } catch (err) {
+    return Response.json(
+      { message: "register has UnKnown internal server error !!" },
+      { status: 500 }
+    );
+  }
 }
