d9421234d6cc730f93d9026de910e2fb5d319996
marzieh <marziehmonshizade@yahoo.com>
2024-06-07 21:32:18 +0330

    change user panel profile and add switch theme button

--- src/components/modules/panelUser/pUserNavbar.tsx
+++ src/components/modules/panelUser/pUserNavbar.tsx
@@ -1,16 +1,14 @@
-import { authUser } from "@/utils/serverHelpers";
-import { IoIosSunny, IoMdNotificationsOutline } from "react-icons/io";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import ThemeSwitch from "../themeSwitch/themeSwitch";
 
 type Props = { userName: string; role: string };
 
 async function PanelUserNavbar({ userName, role }: Props) {
-  const user = await authUser();
-
   return (
     <div className="flex items-center justify-between bg-slate-50 dark:bg-gray-700">
       <div className="flex items-center gap-2">
         <img
-          src="https://img.freepik.com/free-photo/woman-with-beauty-face-clean-skin_186202-5677.jpg"
+          src="/images/userProfile.jpg"
           alt="profile"
           className="w-14 h-14 rounded-full"
         />
@@ -20,11 +18,11 @@ async function PanelUserNavbar({ userName, role }: Props) {
         </div>
       </div>
       <div className="flex items-center gap-2">
-        <div className="w-10 h-10 bg-[#711d1c0f] p-2 rounded-xl flex justify-center items-center">
+        <div className="w-10 h-10 bg-[#F0EDEF] p-2 rounded-xl flex justify-center items-center">
           <IoMdNotificationsOutline className="w-6 h-6 icon" />
         </div>
-        <button className="w-10 h-10 bg-[#711d1c0f] p-2 rounded-xl flex justify-center items-center">
-          <IoIosSunny className="w-6 h-6 icon" />
+        <button className="w-10 h-10 bg-[#F0EDEF] p-2 rounded-xl flex justify-center items-center">
+          <ThemeSwitch />
         </button>
       </div>
     </div>
