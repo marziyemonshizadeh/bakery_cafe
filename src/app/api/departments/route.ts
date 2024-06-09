4525c5b57964a5ca8c28fcdbaa88b6eb65f374c7
marzieh <marziehmonshizade@yahoo.com>
2024-06-09 18:29:18 +0330

    add api: coding departments api

---
+++ src/app/api/departments/route.ts
@@ -0,0 +1,26 @@
+import departmentModel from "@/models/department";
+import connectToDB from "@/utils/db";
+
+export async function POST(request: Request) {
+  try {
+    connectToDB();
+    const body = await request.json();
+    const { title } = body;
+
+    if (!title.trim()) {
+      return Response.json(
+        { message: "Data is not valid !!" },
+        { status: 422 }
+      );
+    }
+    const department = await departmentModel.create({
+      title,
+    });
+    return Response.json(
+      { message: "department created successfully :))", data: department },
+      { status: 200 }
+    );
+  } catch (error) {
+    return Response.json({ message: error }, { status: 500 });
+  }
+}
