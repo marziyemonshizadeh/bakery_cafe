65d6553ce420cf49b142378c64bbb3532134930d
marzieh <marziehmonshizade@yahoo.com>
2024-08-29 15:58:40 +0330

    feature: add toploader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import ScrollToTop from "@/components/modules/scrollToTop/scrollToTop";
 import Providers from "@/lib/ReactQueryClientProvider";
 import AosInit from "@/utils/aos";
 import ThemeProv from "@/utils/theme";
+import NextTopLoader from "nextjs-toploader";
 import "./globals.css";
 
 export const metadata = {
@@ -26,6 +27,21 @@ export default function RootLayout({
             {/* scrollbar-thin scrollbar-thumb-[#413a2d] dark:scrollbar-thumb-[#D1B48C] scrollbar-track-[#D1B48C] dark:scrollbar-track-[#413a2d] h-[100svh]overflow-y-scroll */}
             <div className="static">
               <AosInit />
+              <NextTopLoader
+                color="#D1B48C"
+                initialPosition={0.08}
+                crawlSpeed={200}
+                height={3}
+                crawl={true}
+                showSpinner={true}
+                easing="ease"
+                speed={200}
+                shadow="0 0 10px #431407,0 0 5px #431407"
+                template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+                zIndex={1600}
+                showAtBottom={false}
+              />
               {children}
               <ScrollToTop />
             </div>
